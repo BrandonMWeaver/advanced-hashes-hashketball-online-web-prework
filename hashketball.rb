40c213f9252b2026d1f85d121f7f6ff6ab19ef0a
Brandon Weaver <a7xncob@gmail.com>
2019-10-24 01:26:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -12,9 +12,7 @@ def game_hash
           assists: 12,
           steals: 3,
           blocks: 1,
-          slam_dunks: 1 }
-      ],
-      [
+          slam_dunks: 1 },
         { player_name: "Reggie Evans",
           number: 30,
           shoe: 14,
@@ -23,9 +21,7 @@ def game_hash
           assists: 12,
           steals: 12,
           blocks: 12,
-          slam_dunks: 7 }
-      ],
-      [
+          slam_dunks: 7 },
         { player_name: "Brook Lopez",
           number: 11,
           shoe: 17,
@@ -34,9 +30,7 @@ def game_hash
           assists: 10,
           steals: 3,
           blocks: 1,
-          slam_dunks: 15 }
-      ],
-      [
+          slam_dunks: 15 },
         { player_name: "Mason Plumlee",
           number: 1,
           shoe: 19,
@@ -45,9 +39,7 @@ def game_hash
           assists: 6,
           steals: 3,
           blocks: 8,
-          slam_dunks: 5 }
-      ],
-      [
+          slam_dunks: 5 },
         { player_name: "Jason Terry",
           number: 31,
           shoe: 15,
@@ -71,9 +63,7 @@ def game_hash
           assists: 1,
           steals: 2,
           blocks: 7,
-          slam_dunks: 2 }
-      ],
-      [
+          slam_dunks: 2 },
         { player_name: "Bismack Biyombo",
           number: 0,
           shoe: 16,
@@ -82,9 +72,7 @@ def game_hash
           assists: 7,
           steals: 22,
           blocks: 15,
-          slam_dunks: 10 }
-      ],
-      [
+          slam_dunks: 10 },
         { player_name: "DeSagna Diop",
           number: 2,
           shoe: 14,
@@ -93,9 +81,7 @@ def game_hash
           assists: 12,
           steals: 4,
           blocks: 5,
-          slam_dunks: 5 }
-      ],
-      [
+          slam_dunks: 5 },
         { player_name: "Ben Gordon",
           number: 8,
           shoe: 15,
@@ -104,9 +90,7 @@ def game_hash
           assists: 2,
           steals: 1,
           blocks: 1,
-          slam_dunks: 0 }
-      ],
-      [
+          slam_dunks: 0 },
         { player_name: "Kemba Walker",
           number: 33,
           shoe: 15,
