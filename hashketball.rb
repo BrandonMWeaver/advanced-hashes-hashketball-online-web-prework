b6ac633455e96defa0ac3e3650ae48eb78155086
Brandon Weaver <a7xncob@gmail.com>
2019-10-24 01:12:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,36 @@
 def game_hash
-  hash = {}
+  hash = {
+    home: {
+      team_name: "",
+      colors: [],
+      players: [
+        { player_name: "",
+          number: 0,
+          shoe: 0,
+          points: 0,
+          rebounds: 0,
+          assists: 0,
+          steals: 0,
+          blocks: 0,
+          slam_dunks: 0
+      ]
+    },
+    away: {
+      team_name: "",
+      colors: [],
+      players: [
+        { player_name: "",
+          number: 0,
+          shoe: 0,
+          points: 0,
+          rebounds: 0,
+          assists: 0,
+          steals: 0,
+          blocks: 0,
+          slam_dunks: 0
+      ]
+    }
+  }
   
   return hash
 end
