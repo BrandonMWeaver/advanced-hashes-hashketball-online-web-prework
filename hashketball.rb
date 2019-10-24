dd8645323c0952eb7d3baa4c749fd671e33f042e
Brandon Weaver <a7xncob@gmail.com>
2019-10-24 01:46:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -112,7 +112,6 @@ def num_points_scored(player)
   
   index = 0
   while index < hash[:home][:players].size do
-    puts hash[:home][:players][index][:points]
     if hash[:home][:players][index][:player_name] == player
       return hash[:home][:players][index][:points]
     end
